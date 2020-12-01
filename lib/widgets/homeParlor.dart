f7e419401da8d5856113e40cc8bddb2fcd09e8f1
Letícia Aragão <leticia.aragao2208@gmail.com>
2020-12-01 02:39:57 -0300

    Tela inicial do salão

--- lib/widgets/homeParlor.dart
+++ lib/widgets/homeParlor.dart
@@ -4,6 +4,8 @@ import 'package:hora_salao/controllers/reservation.dart';
 import 'package:hora_salao/widgets/loading.dart';
 import 'indicator.dart';
 import 'package:random_color/random_color.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class HomeParlor extends StatefulWidget {
   @override
@@ -16,9 +18,14 @@ class _HomeParlorState extends State<HomeParlor> {
   int touchedIndex;
   RandomColor _randomColor = RandomColor();
   Map selectedColors = new Map();
+  String month;
 
   @override
   void initState() {
+    initializeDateFormatting('pt_Br');
+    setState(() {
+      month = DateFormat(DateFormat.MONTH, 'pt_Br').format(DateTime.now());
+    });
     reservationController.summary().then((value) {
       setState(() {
         completeSummary = value;
@@ -35,6 +42,35 @@ class _HomeParlorState extends State<HomeParlor> {
         selectedColors[completeSummary['services'][i]] = _color;
       });
     }
+  }
+
+  List<Widget> servicos() {
+    List<Widget> result = new List<Widget>();
+
+    for (var i = 0; i < completeSummary['services'].length; i++) {
+      result.add(
+        FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Text(
+              "${completeSummary['services'][i]}: R\$ ${completeSummary[completeSummary['services'][i]]}",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+        ),
+      );
+      result.add(
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.01,
+        ),
+      );
+    }
+    return result;
   }
 
   List<Widget> indicators() {
@@ -76,6 +112,57 @@ class _HomeParlorState extends State<HomeParlor> {
               )
             : Column(
                 children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Resumo do mês: $month",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Valor Total: R\$ ${completeSummary['total']}",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Porcentagem por serviço:",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
                   AspectRatio(
                     aspectRatio: 0.9,
                     child: Card(
@@ -128,6 +215,66 @@ class _HomeParlorState extends State<HomeParlor> {
                         ],
                       ),
                     ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Número de clientes: ${completeSummary['numberOfReservations']}",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Serviço mais rentável: ${completeSummary['biggestInvestiment']}",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "Valor total por serviço:",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Column(
+                    children: servicos(),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
                   ),
                 ],
               ),
